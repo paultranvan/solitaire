bd0320995a10eeb70e2db3569f20e458c7ad4c07
Gara64 <paul@cozycloud.cc>
2020-03-29 01:01:42 +0100

    Add Empty component

---
+++ src/components/Empty.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Segment } from 'semantic-ui-react'
+
+const Empty = () => {
+  return(
+    <Segment style={{ height: 136, width: 116 }} />
+  )
+}
+
+export default Empty
