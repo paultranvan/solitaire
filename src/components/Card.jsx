b111222d9433a66b9ff38ba07702ed8d2ea3d820
Gara64 <paul@cozycloud.cc>
2020-03-29 20:01:42 +0200

    Refactor Card as HOC

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,43 +1,21 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React from "react"
 import { Image, Segment } from "semantic-ui-react"
 
-class Card extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      cardsPath: "./assets/cards/"
-    }
-  }
+const Card = ({id, value, color, onClick, visible = true}) => {
 
-  render() {
-    const { value, color, onClick, visible } = this.props
+  const cardPath = visible
+    ? "./assets/cards/" + color + "_" + value + ".png"
+    : "./assets/cards/card_back.png"
 
-    const cardPath = visible
-      ? this.state.cardsPath + color + "_" + value + ".png"
-      : this.state.cardsPath + "card_back.png"
-
-    return (
-      <Segment>
-        {onClick !== undefined ? (
-          <Image src={cardPath} alt="" onClick={onClick} size="tiny" />
-        ) : (
-          <Image src={cardPath} alt="" size="tiny" />
-        )}
-      </Segment>
-    )
-  }
-}
-
-Card.propTypes = {
-  id: PropTypes.number,
-  value: PropTypes.number.isRequired,
-  color: PropTypes.string.isRequired,
-  onClick: PropTypes.func
-}
-
-Card.defaultProps = {
-  visible: true
+  return (
+    <Segment>
+      {onClick !== undefined ? (
+        <Image src={cardPath} alt="" onClick={onClick} size="tiny" />
+      ) : (
+        <Image src={cardPath} alt="" size="tiny" />
+      )}
+    </Segment>
+  )
 }
 
 export default Card
